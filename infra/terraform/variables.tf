eb5e679ce106e308a7f288bbd46509447ea18546
kaivalyanidhaye <kaivalyanidhaye@gmail.com>
2025-09-26 18:07:58 +0530

    feat(week1): terraform + compose + service-a seed

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,19 @@
-﻿variable "project" { default = "imoc-poc" }
+﻿variable "aws_region" {
+  type        = string
+  description = "AWS region, e.g., ap-south-1"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Your ED25519/RSA public key contents"
+}
+
+variable "instance_name" {
+  type        = string
+  default     = "itoc-week1"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+}
